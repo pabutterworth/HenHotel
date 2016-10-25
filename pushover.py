192d58a04aaa94c0c3ebe9f40ee3fea96b555af3
pabutterworth <pabutterworth@gmail.com>
2016-10-25 20:46:54 +0100

    Update pushover.py

--- pushover.py
+++ pushover.py
@@ -4,28 +4,16 @@ APP_TOKEN = "ahtdgjqm8w9xxv1v4bzsuwap5c6o7o"
 USER_KEY = "uZ8cbsrmeoMiMJEU6MzHTVKNwrPUr2"
 
 DEBUG = 1
+
 def push(push_text):
     conn = httplib.HTTPSConnection("api.pushover.net:443")
-    conn.request("POST", "/1/messages.json",
-      urllib.urlencode({
-        "token": "APP_TOKEN",
-        "user": "USER_KEY",
-        "message": push_text,
-      }), { "Content-type": "application/x-www-form-urlencoded" })
-    conn.getresponse()
-    return #end of function push
-
-
-def push2(push_text):
-    conn = http.client.HTTPSConnection("api.pushover.net:443")
-    if DEBUG != 1:
+    if !DEBUG
         conn.request("POST", "/1/messages.json",
-          urllib.parse.urlencode({
-            "token": APP_TOKEN,
-            "user": USER_KEY,
-            "title": "PiZero",
+          urllib.urlencode({
+            "token": "APP_TOKEN",
+            "user": "USER_KEY",
+            "title": "Hen Hotel",
             "message": ctime() + " " + push_text,
           }), { "Content-type": "application/x-www-form-urlencoded" })
-    conn.getresponse()
-    print push_text
-    return  # End of function push
+        conn.getresponse()
+    return #end of function push
