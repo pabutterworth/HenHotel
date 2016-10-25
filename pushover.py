a0691d7156a5e786a45617f6ffb090897e6e3a39
pabutterworth <pabutterworth@gmail.com>
2016-10-25 20:05:40 +0100

    Create pushover.py

---
+++ pushover.py
@@ -0,0 +1,16 @@
+import http.client, urllib
+from time import ctime
+APP_TOKEN = "ahtdgjqm8w9xxv1v4bzsuwap5c6o7o"
+USER_KEY = "uZ8cbsrmeoMiMJEU6MzHTVKNwrPUr2"
+
+def push(push_text):
+    conn = http.client.HTTPSConnection("api.pushover.net:443")
+    conn.request("POST", "/1/messages.json",
+      urllib.parse.urlencode({
+        "token": APP_TOKEN,
+        "user": USER_KEY,
+        "title": "PiZero",
+        "message": ctime() + " " + push_text,
+      }), { "Content-type": "application/x-www-form-urlencoded" })
+    conn.getresponse()
+    return  # End of function push
