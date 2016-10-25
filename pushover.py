fc087e2aabda37b952c6202e5f01482d5580108a
pabutterworth <pabutterworth@gmail.com>
2016-10-25 20:33:46 +0100

    Update pushover.py

--- pushover.py
+++ pushover.py
@@ -3,14 +3,17 @@ from time import ctime
 APP_TOKEN = "ahtdgjqm8w9xxv1v4bzsuwap5c6o7o"
 USER_KEY = "uZ8cbsrmeoMiMJEU6MzHTVKNwrPUr2"
 
+DEBUG = 1
+
 def push(push_text):
     conn = http.client.HTTPSConnection("api.pushover.net:443")
-    conn.request("POST", "/1/messages.json",
-      urllib.parse.urlencode({
-        "token": APP_TOKEN,
-        "user": USER_KEY,
-        "title": "PiZero",
-        "message": ctime() + " " + push_text,
-      }), { "Content-type": "application/x-www-form-urlencoded" })
+    if DEBUG != 1 
+        conn.request("POST", "/1/messages.json",
+          urllib.parse.urlencode({
+            "token": APP_TOKEN,
+            "user": USER_KEY,
+            "title": "PiZero",
+            "message": ctime() + " " + push_text,
+          }), { "Content-type": "application/x-www-form-urlencoded" })
     conn.getresponse()
     return  # End of function push
