c49cc861e617354534b081c8dc5967b0608f850c
pabutterworth <pabutterworth@gmail.com>
2016-10-25 20:42:55 +0100

    Update pushover.py

--- pushover.py
+++ pushover.py
@@ -4,8 +4,19 @@ APP_TOKEN = "ahtdgjqm8w9xxv1v4bzsuwap5c6o7o"
 USER_KEY = "uZ8cbsrmeoMiMJEU6MzHTVKNwrPUr2"
 
 DEBUG = 1
+def push(push_text)
+    conn = httplib.HTTPSConnection("api.pushover.net:443")
+    conn.request("POST", "/1/messages.json",
+      urllib.urlencode({
+        "token": "APP_TOKEN",
+        "user": "USER_KEY",
+        "message": push_text,
+      }), { "Content-type": "application/x-www-form-urlencoded" })
+    conn.getresponse()
+    return #end of function push
 
-def push(push_text):
+
+def push2(push_text):
     conn = http.client.HTTPSConnection("api.pushover.net:443")
     if DEBUG != 1:
         conn.request("POST", "/1/messages.json",
