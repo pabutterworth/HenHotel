1c4e46fbb23a7e2bd63bb1799fdef818812bfc02
pabutterworth <pabutterworth@gmail.com>
2016-11-12 18:02:50 +0000

    Update distance.py

--- distance.py
+++ distance.py
@@ -12,24 +12,25 @@ GPIO.setup(ECHO,GPIO.IN)
 
 GPIO.output(TRIG, False)
 print "Waiting For Sensor To Settle"
-time.sleep(2)
+while True:
+  time.sleep(0.5)
 
-GPIO.output(TRIG, True)
-time.sleep(0.00001)
-GPIO.output(TRIG, False)
+  GPIO.output(TRIG, True)
+  time.sleep(0.00001)
+  GPIO.output(TRIG, False)
 
-while GPIO.input(ECHO)==0:
-  pulse_start = time.time()
+  while GPIO.input(ECHO)==0:
+    pulse_start = time.time()
 
-while GPIO.input(ECHO)==1:
-  pulse_end = time.time()
+  while GPIO.input(ECHO)==1:
+    pulse_end = time.time()
 
-pulse_duration = pulse_end - pulse_start
+  pulse_duration = pulse_end - pulse_start
 
-distance = pulse_duration * 17150
+  distance = pulse_duration * 17150
 
-distance = round(distance, 2)
+  distance = round(distance, 2)
 
-print "Distance:",distance,"cm"
+  print "Distance:",distance,"cm"
 
 GPIO.cleanup()
