cc0e62221b7166341ee9ba13daa07e99944f1e2d
pabutterworth <pabutterworth@gmail.com>
2016-11-18 17:23:39 +0000

    Update main.py

--- main.py
+++ main.py
@@ -26,14 +26,14 @@ def timer():
 
 
 def main():
-   log = logging.getLogger(__name__)
+    log = logging.getLogger(__name__)
 
-   log.setLevel(logging.DEBUG)
+    log.setLevel(logging.DEBUG)
 
-   handler = logging.handlers.SysLogHandler(address = '/dev/log')
+    handler = logging.handlers.SysLogHandler(address = '/dev/log')
 
-   formatter = logging.Formatter('%(module)s.%(funcName)s: %(message)s')
-   handler.setFormatter(formatter)
+    formatter = logging.Formatter('%(module)s.%(funcName)s: %(message)s')
+    handler.setFormatter(formatter)
 
     log.addHandler(handler)
     today=date.today()
