66b0c881c76bd70b4de082a684561baecf4ae2e9
pabutterworth <pabutterworth@gmail.com>
2016-11-20 19:00:45 +0000

    Update doorcontrol.py

--- doorcontrol.py
+++ doorcontrol.py
@@ -20,8 +20,8 @@ REVERSE = 2
 STOPMOTOR = 3
 
 MAX_TIME = 30
-CLOSE_TIME = 10 #time to take to close
-OPEN_TIME = 10 #time to open  
+CLOSE_TIME = 20 #time to take to close
+OPEN_TIME = 20 #time to open  
 
 OPEN_DISTANCE = 70
 CLOSED_DISTANCE = 20
